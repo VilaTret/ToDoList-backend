cdad44179355c23d5c89624312941ef8d3fc6aa5
Dmytro Danchuk <danchuk2002@gmail.com>
2024-06-20 13:21:33 +0300

    Add connect to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { resolve } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [resolve(__dirname, './**/**/entities/*{.ts,.js}')],
+
+      logging: ['error'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
