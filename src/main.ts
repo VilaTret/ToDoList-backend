c4f78295954e34ecc1ae940f9b4f87338290db03
Dmytro Danchuk <danchuk2002@gmail.com>
2024-06-20 14:32:00 +0300

    Add CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.APP_PORT || 3000;
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(PORT, () => {
     console.log(`App started on url: http://localhost:${PORT}`);
   });
